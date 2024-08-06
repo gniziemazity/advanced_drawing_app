db10d632d780192b9dcb50999b5320ffe79e4172
Rahul Thakare <16685565+bantya@users.noreply.github.com>
2024-08-06 23:06:06 +0530

    Added height-width constrain checkbox

--- panels/propertiesPanel.js
+++ panels/propertiesPanel.js
@@ -22,6 +22,12 @@ class PropertiesPanel {
          }),
        );
       this.holderDiv.appendChild(
+         createInputWithLabel("Constrain", {
+           type: "checkbox",
+           id: "constrainDimensions"
+         }),
+       );
+      this.holderDiv.appendChild(
          createInputWithLabel("Height", {
            type: "number",
            onchange: "PropertiesPanel.changeHeight(this.value)",
@@ -111,25 +117,57 @@ class PropertiesPanel {
    }
 
    static changeWidth(value) {
+      const aspectRatio =
+        getProperty(width, "data-width") / getProperty(height, "data-height");
       const fixedValue=Math.max(Number(value),1);
+      const newHeight = fixedValue / aspectRatio;
       width.value=fixedValue;
+
       shapes
          .filter((s) => s.selected)
          .forEach((s) => s.setWidth(Number(fixedValue)));
 
+      if (constrainDimensions.checked) {
+         height.value = Math.round(newHeight);
+
+         shapes
+            .filter((s) => s.selected)
+            .forEach((s) => s.setHeight(newHeight));
+
+         setProperty(height, "data-height", newHeight);
+      }
+
       updateHistory(shapes);
       drawShapes(shapes);
+
+      setProperty(width, 'data-width', fixedValue);
    }
 
    static changeHeight(value) {
+      const aspectRatio =
+        getProperty(width, "data-width") / getProperty(height, "data-height");
       const fixedValue=Math.max(Number(value),1);
+      const newWidth = aspectRatio * fixedValue;
+
       height.value=fixedValue;
       shapes
          .filter((s) => s.selected)
          .forEach((s) => s.setHeight(Number(value)));
 
+      if (constrainDimensions.checked) {
+         width.value = Math.round(newWidth);
+
+         shapes
+            .filter((s) => s.selected)
+            .forEach((s) => s.setWidth(newWidth));
+
+            setProperty(width, "data-width", newWidth);
+      }
+
       updateHistory(shapes);
       drawShapes(shapes);
+
+      setProperty(height, "data-height", fixedValue);
    }
 
    static previewFillColor(value) {
@@ -290,7 +328,9 @@ class PropertiesPanel {
          x.placeholder = newProperties.x ? "" : placeholderText;
          y.placeholder = newProperties.y ? "" : placeholderText;
          width.placeholder = newProperties.width ? "" : placeholderText;
+         setProperty(width, 'data-width', newProperties.width);
          height.placeholder = newProperties.height ? "" : placeholderText;
+         setProperty(height, 'data-height', newProperties.height);
          fillColor.placeholder = newProperties.fillColor ? "" : placeholderText;
          fill.placeholder = newProperties.fill ? "" : placeholderText;
          strokeColor.placeholder = newProperties.strokeColor ? "" : placeholderText;
