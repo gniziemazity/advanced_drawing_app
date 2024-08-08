81851bfce7bc4abd85fe2fe13dd0fefb51f3d25a
Michael <mgolden@revspringinc.com>
2024-08-08 11:05:43 -0500

    fix: console error typo

--- shapes/path.js
+++ shapes/path.js
@@ -43,7 +43,7 @@ class Path extends Shape {
       const flip = Math.sign(width) != Math.sign(this.size.width) ? -1 : 1;
       const eps = 0.0001;
       if (size.width == 0) {
-         console.err("Size 0 problem!");
+         console.error("Size 0 problem!");
       }
       const _width = size.width == 0 ? eps : size.width;
       const ratio = (flip * Math.abs(width)) / _width;
@@ -58,7 +58,7 @@ class Path extends Shape {
       const flip = Math.sign(height) != Math.sign(this.size.height) ? -1 : 1;
       const eps = 0.0001;
       if (size.height == 0) {
-         console.err("Size 0 problem!");
+         console.error("Size 0 problem!");
       }
       const _height = size.height == 0 ? eps : size.height;
       const ratio = (flip * Math.abs(height)) / _height;
