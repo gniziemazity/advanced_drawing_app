7d2c3de178f953842a61435b1bd25b1a6e8715f2
OneDios <onedios2019@gmail.com>
2024-08-09 04:56:15 +0530

    added menu toolbars

--- main.js
+++ main.js
@@ -18,12 +18,61 @@ let currentShape = null
 let clipboard = null
 
 const propertiesPanel = new PropertiesPanel()
-const toolsPanel = new ToolsPanel(toolsHolder)
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	const uiManager = new UIManager()
 	const tools = new Tools()
+
+	const menu = new Toolbar()
+	const undoButton = new Button("<i class='bx bx-undo bx-sm' style='color:#ffffff'></i>")
+	undoButton.action = (e) => {
+		HistoryTools.undo()
+	}
+
+	const redoButton = new Button("<i class='bx bx-redo bx-sm' style='color:#ffffff'></i>")
+	redoButton.action = (e) => {
+		HistoryTools.redo()
+	}
+
+	const saveButton = new Button("<i class='bx bx-save bx-sm' style='color:#ffffff'></i>")
+	saveButton.action = (e) => {
+		DocumentTools.save()
+	}
+
+	const loadButton = new Button("Load")
+	loadButton.action = (e) => {
+		DocumentTools.load()
+	}
+
+	const exportButton = new Button("Export")
+	exportButton.action = (e) => {
+		DocumentTools.do_export()
+	}
+
+	const copyButton = new Button("<i class='bx bxs-copy bx-sm' style='color:#ffffff' ></i>")
+	copyButton.action = (e) => {
+		EditingTools.duplicate()
+	}
+
+	const selectAllButton = new Button("<i class='bx bx-select-multiple bx-sm' style='color:#ffffff' ></i>")
+	selectAllButton.action = (e) => {
+		EditingTools.selectAll()
+	}
+
+	const deleteButton = new Button("<i class='bx bx-trash bx-sm'></i>")
+	deleteButton.action = (e) => {
+		EditingTools.delete()
+	}
+
+	menu.add(undoButton)
+	menu.add(redoButton)
+	menu.add(saveButton)
+	menu.add(loadButton)
+	menu.add(exportButton)
+	menu.add(copyButton)
+	menu.add(selectAllButton)
+	menu.add(deleteButton)
 
 	for (let key in ShapeTools.tools) {
 
@@ -38,6 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		tools.add(shapeTool)
 	}
 
+	uiManager.add(menu)
 	uiManager.attachTools(tools)
 	uiManager.renderAll()
 })
