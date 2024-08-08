085b7133b59ec615555a7ec760b8a09b3eb50db5
OneDios <onedios2019@gmail.com>
2024-08-09 02:39:43 +0530

    updated panel properties ui

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const SHOW_HIT_REGIONS = false;
-const RECTANGULAR_SELECTION_MODE = "intersection"; // "intersection" or "containment"
+const SHOW_HIT_REGIONS = false
+const RECTANGULAR_SELECTION_MODE = "intersection" // "intersection" or "containment"
 
 const STAGE_PROPERTIES = {
 	width: 600,
 	height: 480,
-};
+}
 
 const viewport = new Viewport(
 	canvasHolder,
 	STAGE_PROPERTIES,
 	SHOW_HIT_REGIONS
-);
+)
 
-let shapes = [];
-let gizmos = [];
-let currentShape = null;
-let clipboard = null;
+let shapes = []
+let gizmos = []
+let currentShape = null
+let clipboard = null
 
-const propertiesPanel = new PropertiesPanel(propertiesHolder);
-const toolsPanel = new ToolsPanel(toolsHolder);
+const propertiesPanel = new PropertiesPanel()
+const toolsPanel = new ToolsPanel(toolsHolder)
