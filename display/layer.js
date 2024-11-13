d03cc8f01b6dfcab0be177c04ec8d8166b635a5f
iyusuf <punkgts40@gmail.com>
2024-11-13 21:26:38 +0100

    scale to canvas.ctx scale in one place only

--- display/layer.js
+++ display/layer.js
@@ -17,6 +17,8 @@ class Layer {
 		this.canvas.style.height = canvasHeight + "px";
 		this.canvas.width = Math.floor(canvasWidth * pixelRatio);
 		this.canvas.height = Math.floor(canvasHeight * pixelRatio);
+		this.canvas.unscaledWidth = canvasWidth
+		this.canvas.unscaledHeight = canvasHeight
 		this.ctx = this.canvas.getContext("2d", {
 			willReadFrequently: true,
 		});
@@ -204,11 +206,14 @@ class Layer {
 	}
 
 	clearCanvas() {
+		// using canvas.width and .height causes a bug where
+		// on browser zoom out canvas edges are not cleared leaving
+		// artifacts
 		this.ctx.clearRect(
-			-this.canvas.width / 2,
-			-this.canvas.height / 2,
-			this.canvas.width,
-			this.canvas.height
+			-this.canvas.unscaledWidth / 2,
+			-this.canvas.unscaledHeight / 2,
+			this.canvas.unscaledWidth,
+			this.canvas.unscaledHeight
 		);
 	}
 
--- tools/selectTool.js
+++ tools/selectTool.js
@@ -14,14 +14,19 @@ class SelectTool extends GenericTool {
 	addPointerDownListener(e) {
 		if (e.button !== 0) return;
 
-		const startPosition = new Vector(e.offsetX, e.offsetY).scale(window.devicePixelRatio);
+		const startPosition = new Vector(e.offsetX, e.offsetY)
+
+		const startPositionCtxScale = startPosition.scale(
+			window.devicePixelRatio
+		);
 
 		const [r, g, b, a] = viewport.hitTestLayer.ctx.getImageData(
-			startPosition.x,
-			startPosition.y,
+			startPositionCtxScale.x,
+			startPositionCtxScale.y,
 			1,
 			1
 		).data;
+
 
 		const id = (r << 16) | (g << 8) | b;
 		const gizmo = viewport.gizmos.find((g) => g.hasHandle(id));
@@ -56,7 +61,7 @@ class SelectTool extends GenericTool {
 
 			const moveCallback = function (e) {
 				const mousePosition = new Vector(e.offsetX, e.offsetY).scale(window.devicePixelRatio);
-				const diff = Vector.subtract(mousePosition, startPosition).scale(1/window.devicePixelRatio);
+				const diff = Vector.subtract(mousePosition, startPosition).scale(1 / window.devicePixelRatio);
 				mouseDelta = viewport.getAdjustedScale(diff);
 				isDragging = true;
 				selectedShapes.forEach((s, i) => {
